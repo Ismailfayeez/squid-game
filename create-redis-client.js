d6573c9464f25f42effef3f90031301373a41043
mfe526 <145103881+MohammedIsmailFayeez@users.noreply.github.com>
2025-06-18 13:00:31 +0530

    add redis_URL

--- create-redis-client.js
+++ create-redis-client.js
@@ -2,7 +2,7 @@ const { createClient } = require('redis');
 
 const createRedisClient = () => {
     const client = createClient({
-        url: 'rediss://red-d191rt7fte5s73bthkp0:j9XbiexiBXDaqvC7KdAFUKOfsD8R0Cae@oregon-keyvalue.render.com:6379',
+        url: process.env.REDIS_URL,
     });
     client.on('error', (err) => console.log('Redis Client Error', err));
     client.connect();
