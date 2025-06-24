9cf7f5085b473dcd4ba7e8e3aad69258a4930211
mfe526 <145103881+MohammedIsmailFayeez@users.noreply.github.com>
2025-06-24 13:02:30 +0530

    preload images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import Cookies from 'js-cookie'
-import { Session } from './components/Session'
-import { Login } from './components/Login'
-import { TOKEN_HEADER_KEY } from '../constants'
-import './App.css'
-import { Score } from './components/Score'
+import { useState } from 'react';
+import Cookies from 'js-cookie';
+import { Session } from './components/Session';
+import { Login } from './components/Login';
+import { TOKEN_HEADER_KEY } from '../constants';
+import './App.css';
 
 function App() {
-    const token = Cookies.get(TOKEN_HEADER_KEY)
-    const [location, setLocation] = useState(token ? 'game' : 'login')
+    const token = Cookies.get(TOKEN_HEADER_KEY);
+    const [location, setLocation] = useState(token ? 'game' : 'login');
     return (
         <>
             {location === 'login' && <Login setLocation={setLocation} />}
             {location === 'game' && <Session setLocation={setLocation} />}
         </>
-    )
+    );
 }
 
-export default App
+export default App;
