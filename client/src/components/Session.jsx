69ccf02ccc69fc26144750467a6e187c5b15d833
mfe526 <145103881+MohammedIsmailFayeez@users.noreply.github.com>
2025-06-24 09:21:54 +0530

    camera reognize horizontally on mobile device

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -1,116 +1,131 @@
-import React, { useEffect, useReducer, useRef } from "react";
-import { Game } from "./Game";
-import { gameReducer } from "../reducer";
-import { handleGesture } from "../gesture-recognizer/handleGesture";
-import { Room } from "./Room";
-import { Score } from "./Score";
-import ThemeSong from "../assets/sgTheme.mp3";
-import FlyMoon from "../assets/flyMoon.mp3";
-import Notify from "../assets/notify.mp3";
+import React, { useEffect, useReducer, useRef } from 'react';
+import { Game } from './Game';
+import { gameReducer } from '../reducer';
+import { handleGesture } from '../gesture-recognizer/handleGesture';
+import { Room } from './Room';
+import { Score } from './Score';
+import ThemeSong from '../assets/sgTheme.mp3';
+import FlyMoon from '../assets/flyMoon.mp3';
+import Notify from '../assets/notify.mp3';
 import {
-  FINISHED,
-  GAME_STATUSES,
-  MOVE,
-  REMOVE_TOKEN,
-  SCORE_BOARD,
-  START_GAME,
-} from "../constants";
-import { useInputSource } from "../hooks/useInputSource";
+    FINISHED,
+    GAME_STATUSES,
+    MOVE,
+    REMOVE_TOKEN,
+    SCORE_BOARD,
+    START_GAME,
+} from '../constants';
+import { useInputSource } from '../hooks/useInputSource';
 
 export const Session = ({ setLocation }) => {
-  const [data, dispatch] = useReducer(gameReducer, {});
-  const { status, gameStat: { players = {} } = {}, code, me } = data;
+    const [data, dispatch] = useReducer(gameReducer, {});
+    const { status, gameStat: { players = {} } = {}, code, me } = data;
 
-  const socketRef = useRef(null);
+    const socketRef = useRef(null);
 
-  const playersStatus = Object.keys(players).map((name) => ({
-    name,
-    status: players[name]?.status,
-  }));
-  const currentPlayerStatus = players?.[me?.name]?.status;
+    const playersStatus = Object.keys(players).map((name) => ({
+        name,
+        status: players[name]?.status,
+    }));
+    const currentPlayerStatus = players?.[me?.name]?.status;
 
-  const { inputMode, isInputSrcConfirmed, videoRef } = useInputSource(
-    status,
-    currentPlayerStatus
-  );
-  const { MODE, VITE_API_URL } = import.meta.env;
+    const {
+        inputMode,
+        isInputSrcConfirmed,
+        canvasRef: videoRef,
+    } = useInputSource(status, currentPlayerStatus);
+    const { MODE, VITE_API_URL } = import.meta.env;
 
-  const protocol = MODE === "production" ? "wss" : "ws";
-  const domain = MODE === "production" ? VITE_API_URL : "localhost:3000";
-  const baseUrl = `${protocol}://${domain}`;
+    const protocol = MODE === 'production' ? 'wss' : 'ws';
+    const domain = MODE === 'production' ? VITE_API_URL : 'localhost:3000';
+    const baseUrl = `${protocol}://${domain}`;
 
-  useEffect(() => {
-    socketRef.current = new WebSocket(`${baseUrl}/game`);
+    useEffect(() => {
+        socketRef.current = new WebSocket(`${baseUrl}/game`);
 
-    socketRef.current.onmessage = ({ data }) =>
-      dispatch({ data: JSON.parse(data) });
+        socketRef.current.onmessage = ({ data }) =>
+            dispatch({ data: JSON.parse(data) });
 
-    socketRef.current.onopen = () => {
-      console.log("open");
+        socketRef.current.onopen = () => {
+            console.log('open');
+        };
+        socketRef.current.onclose = () => {
+            document.cookie = REMOVE_TOKEN;
+        };
+        socketRef.current.onerror = () => {
+            document.cookie = REMOVE_TOKEN;
+            setLocation('login');
+        };
+
+        return () => socketRef.current.close();
+    }, []);
+
+    const handleStartGame = () =>
+        socketRef.current.send(JSON.stringify({ action: START_GAME }));
+
+    const handleMove = () => {
+        socketRef.current.send(JSON.stringify({ action: MOVE }));
+        const walkSound = document.getElementById('walk');
+        walkSound.playbackRate = 16;
+        walkSound.play();
+        return 1;
     };
-    socketRef.current.onclose = () => {
-      document.cookie = REMOVE_TOKEN;
+
+    const handleFinish = () =>
+        socketRef.current.send(JSON.stringify({ action: FINISHED }));
+
+    const handleInput = (inputMode) => {
+        console.log();
+        if (inputMode === 'camera')
+            return handleGesture(videoRef.current, handleMove);
+        else return handleMove();
     };
-    socketRef.current.onerror = () => {
-      document.cookie = REMOVE_TOKEN;
-      setLocation("login");
+    const handleExit = () => {
+        document.cookie = REMOVE_TOKEN;
+        setLocation('login');
     };
-
-    return () => socketRef.current.close();
-  }, []);
-
-  const handleStartGame = () =>
-    socketRef.current.send(JSON.stringify({ action: START_GAME }));
-
-  const handleMove = () => {
-    socketRef.current.send(JSON.stringify({ action: MOVE }));
-    const walkSound = document.getElementById("walk");
-    walkSound.playbackRate = 16;
-    walkSound.play();
-    return 1;
-  };
-
-  const handleFinish = () =>
-    socketRef.current.send(JSON.stringify({ action: FINISHED }));
-
-  const handleInput = (inputMode) => {
-    if (inputMode === "camera")
-      return handleGesture(videoRef.current, handleMove);
-    else return handleMove();
-  };
-  const handleExit = () => {
-    document.cookie = REMOVE_TOKEN;
-    setLocation("login");
-  };
-  const render = () => {
-    if (status === SCORE_BOARD)
-      return <Score players={players} handleExit={handleExit} />;
-    else if (isInputSrcConfirmed != null && GAME_STATUSES.includes(status))
-      return (
-        <Game
-          data={data}
-          inputMode={inputMode}
-          handleInput={handleInput}
-          handleFinish={handleFinish}
-        />
-      );
-    else
-      return (
-        <Room
-          players={playersStatus}
-          status={status}
-          code={code}
-          handleStart={handleStartGame}
-          handleExit={handleExit}
-        />
-      );
-  };
-  return (
-    <>
-      <audio src={ThemeSong} id="theme" loop />
-      <audio src={FlyMoon} id="fly-moon" loop />
-      <audio src={Notify} id="notify" />
-      {render()}
-    </>
-  );
+    const render = () => {
+        if (status === SCORE_BOARD)
+            return <Score players={players} handleExit={handleExit} />;
+        else if (isInputSrcConfirmed != null && GAME_STATUSES.includes(status))
+            return (
+                <Game
+                    data={data}
+                    inputMode={inputMode}
+                    handleInput={handleInput}
+                    handleFinish={handleFinish}
+                />
+            );
+        else
+            return (
+                <Room
+                    players={playersStatus}
+                    status={status}
+                    code={code}
+                    handleStart={handleStartGame}
+                    handleExit={handleExit}
+                />
+            );
+    };
+    return (
+        <>
+            <video
+                id="camera-video"
+                className="webcam"
+                autoPlay
+                playsInline
+                muted
+            />
+            <canvas
+                id="camera-canvas"
+                className="webcam"
+                width={480}
+                height={640}
+            />
+            <audio src={ThemeSong} id="theme" loop />
+            <audio src={FlyMoon} id="fly-moon" loop />
+            <audio src={Notify} id="notify" />
+            {render()}
+        </>
+    );
 };
--- client/src/hooks/useInputSource.js
+++ client/src/hooks/useInputSource.js
@@ -1,55 +1,73 @@
-import { useEffect, useRef, useState } from 'react'
-import { fetchCameraStream } from '../utils/fetchCameraStream'
-import { ACTIVE_GAME_STATUSES, CLOSED_GAME_STATUSES } from '../constants'
+import { useEffect, useRef, useState } from 'react';
+import { fetchCameraStream } from '../utils/fetchCameraStream';
+import { ACTIVE_GAME_STATUSES, CLOSED_GAME_STATUSES } from '../constants';
 
 export const useInputSource = (gameStatus, playerStatus) => {
-    const [inputMode, setInputMode] = useState('keys')
-    const [isInputSrcConfirmed, setIsInputSrcConfirmed] = useState(null)
-    const videoRef = useRef(null)
-    const streamRef = useRef(null)
+    const [inputMode, setInputMode] = useState('keys');
+    const [isInputSrcConfirmed, setIsInputSrcConfirmed] = useState(null);
+
+    const videoRef = useRef(null);
+    const canvasRef = useRef(null);
+    const streamRef = useRef(null);
+    const animeFrameRef = useRef(null);
 
     const getInputSourceConfirmation = async () => {
         try {
-            videoRef.current = document.createElement('video')
-            videoRef.current.classname = 'webcam'
-            videoRef.current.autoPlay = true
-            videoRef.current.playsInline = true
-            streamRef.current = await fetchCameraStream(videoRef.current)
+            // get elements
+            videoRef.current = document.getElementById('camera-video');
+            canvasRef.current = document.getElementById('camera-canvas');
+            streamRef.current = await fetchCameraStream(videoRef.current);
 
             videoRef.current.addEventListener('loadeddata', () => {
-                setInputMode('camera')
-                setIsInputSrcConfirmed(true)
-            })
-        } catch {
-            setInputMode('keys')
-            setIsInputSrcConfirmed(true)
-        }
-    }
+                setInputMode('camera');
+                setIsInputSrcConfirmed(true);
+            });
 
-    const removeInputSrc = () =>
-        streamRef.current?.getTracks()?.forEach((track) => track.stop())
+            // create canvas
+            const { width, height } = canvasRef.current;
+            const ctx = canvasRef.current.getContext('2d');
+
+            const createCanvasVideo = () => {
+                ctx.clearRect(0, 0, width, height);
+                ctx.save();
+                if (window.innerWidth <= 480) {
+                    ctx.translate(width, 0);
+                    ctx.rotate((90 * Math.PI) / 180);
+                }
+                ctx.drawImage(videoRef.current, 0, 0, height, width);
+                ctx.restore();
+                animeFrameRef.current =
+                    requestAnimationFrame(createCanvasVideo);
+            };
+
+            createCanvasVideo();
+        } catch {
+            setInputMode('keys');
+            setIsInputSrcConfirmed(true);
+        }
+    };
+    const removeVideoCapture = () => {
+        streamRef.current?.getTracks()?.forEach((track) => track.stop());
+        cancelAnimationFrame(animeFrameRef.current);
+    };
 
     useEffect(() => {
-        if (gameStatus === undefined || playerStatus === undefined) return
+        if (gameStatus === undefined || playerStatus === undefined) return;
 
         if (ACTIVE_GAME_STATUSES.includes(gameStatus))
             if (playerStatus === 'ALIVE')
-                !isInputSrcConfirmed && getInputSourceConfirmation()
+                !isInputSrcConfirmed && getInputSourceConfirmation();
             else {
-                setIsInputSrcConfirmed(false)
-                removeInputSrc()
+                setIsInputSrcConfirmed(false);
+                removeVideoCapture();
             }
         else if (CLOSED_GAME_STATUSES.includes(gameStatus)) {
-            setIsInputSrcConfirmed(false)
-            removeInputSrc()
+            setIsInputSrcConfirmed(false);
+            removeVideoCapture();
         }
 
-        const removeEvent = () => {
-            videoRef.current.removeEventListener('loadeddata')
-        }
+        return () => removeVideoCapture;
+    }, [gameStatus, playerStatus, isInputSrcConfirmed]);
 
-        return () => removeEvent
-    }, [gameStatus, playerStatus, isInputSrcConfirmed])
-
-    return { inputMode, isInputSrcConfirmed, videoRef }
-}
+    return { inputMode, isInputSrcConfirmed, canvasRef };
+};
