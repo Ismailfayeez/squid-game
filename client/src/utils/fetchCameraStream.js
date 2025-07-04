b516d367b0e03195fa62bbcc935de991fe644e11
mfe526 <145103881+MohammedIsmailFayeez@users.noreply.github.com>
2025-07-04 16:32:32 +0530

    user media constraint error fix

--- client/src/utils/fetchCameraStream.js
+++ client/src/utils/fetchCameraStream.js
@@ -1,9 +1,6 @@
 export const fetchCameraStream = async (video) => {
     const stream = await navigator.mediaDevices.getUserMedia({
-        video: {
-            facingMode: { exact: 'user' },
-        },
-        audio: false,
+        video: true,
     });
     video.srcObject = stream;
     video.play();
